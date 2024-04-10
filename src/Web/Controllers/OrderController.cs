7b7c89e8457b6e64f1166abcfc860feab0039c37
UNGROUP\nilsua <NilsuACAR@ugm.com.tr>
2024-04-10 05:09:43 +0300

    Added actions and authorize.

--- src/Web/Controllers/OrderController.cs
+++ src/Web/Controllers/OrderController.cs
@@ -4,6 +4,9 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.eShopWeb.Web.Features.MyOrders;
 using Microsoft.eShopWeb.Web.Features.OrderDetails;
+using Microsoft.eShopWeb.Web.Features.UpdateUserStatus;
+using Microsoft.eShopWeb.Web.Features.UserOrderDetails;
+using Microsoft.eShopWeb.Web.Features.UserOrders;
 
 namespace Microsoft.eShopWeb.Web.Controllers;
 
@@ -28,6 +31,7 @@ public class OrderController : Controller
         return View(viewModel);
     }
 
+
     [HttpGet("{orderId}")]
     public async Task<IActionResult> Detail(int orderId)
     {
@@ -40,5 +44,49 @@ public class OrderController : Controller
         }
 
         return View(viewModel);
+    }
+
+
+
+    [Authorize(Roles = "Administrators")]
+    [HttpGet]
+    public async Task<IActionResult> UserOrders()
+    {
+        Guard.Against.Null(User?.Identity?.Name, nameof(User.Identity.Name));
+        var viewModel = await _mediator.Send(new GetUserOrders());
+
+        return View(viewModel);
+    }
+
+
+    [Authorize(Roles = "Administrators")]
+    [HttpGet("{orderId}")]
+    public async Task<IActionResult> UserOrderDetail(int orderId)
+    {
+        var viewModel = await _mediator.Send(new GetUserOrderDetails(orderId));
+
+        if (viewModel == null)
+        {
+            return BadRequest("No such order found for this user.");
+        }
+
+        return View(viewModel);
+    }
+
+
+    [Authorize(Roles = "Administrators")]
+    [HttpPost]
+    public async Task<IActionResult> ChangeStatusUserOrders(int id, int statusId)
+    {
+        try
+        {
+            await _mediator.Send(new UpdateOrderStatus(id, statusId));
+            return RedirectToAction(nameof(UserOrders));
+        }
+        catch (Exception)
+        {
+            return BadRequest($"StatusId is not found at StatusList.");
+        }
+
     }
 }
