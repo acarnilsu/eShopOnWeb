dfdf380cfa53aba8346df5393c28a9fdc315cb1e
UNGROUP\nilsua <NilsuACAR@ugm.com.tr>
2024-04-10 05:04:06 +0300

    Added specification.

---
+++ src/ApplicationCore/Specifications/AllCustomerOrdersSpecification.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ardalis.Specification;
+using Microsoft.eShopWeb.ApplicationCore.Entities.OrderAggregate;
+
+namespace Microsoft.eShopWeb.ApplicationCore.Specifications;
+public class AllCustomerOrdersSpecification:Specification<Order>
+{
+    public AllCustomerOrdersSpecification()
+    {
+        Query.Include(o => o.OrderItems);
+    }
+}
