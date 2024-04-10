dc9184e2be919d3eb7151852fa956365b44ddf9a
UNGROUP\nilsua <NilsuACAR@ugm.com.tr>
2024-04-10 05:07:30 +0300

    Added update OrderStatus.

---
+++ src/Web/Features/UpdateUserStatus/UpdateOrderStatus.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+
+namespace Microsoft.eShopWeb.Web.Features.UpdateUserStatus;
+
+public class UpdateOrderStatus:IRequest
+{
+    public UpdateOrderStatus(int id, int status)
+    {
+        Id = id;
+        StatusId = status;
+    }
+
+    public int Id { get; set; }
+    public int StatusId { get; set;}
+}
---
+++ src/Web/Features/UpdateUserStatus/UpdateOrderStatusHandler.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Microsoft.eShopWeb.ApplicationCore.Interfaces;
+using NuGet.Protocol.Plugins;
+
+namespace Microsoft.eShopWeb.Web.Features.UpdateUserStatus;
+
+public class UpdateOrderStatusHandler : IRequestHandler<UpdateOrderStatus>
+{
+    private readonly IOrderService _orderService;
+
+    public UpdateOrderStatusHandler(IOrderService orderService)
+    {
+        _orderService = orderService;
+    }
+
+    public async Task Handle(UpdateOrderStatus request, CancellationToken cancellationToken)
+    {
+        await _orderService.ChangeStatusOrderAsync(request.Id, request.StatusId);
+    }
+}
